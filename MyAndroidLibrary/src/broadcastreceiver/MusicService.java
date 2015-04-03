64cfd071bd4b09acf4a8b462f853cb84a5913648
shuwei-csh <shuwei_csh@126.com>
2015-04-03 23:28:34 +0800

    BroadcastReceive和Service综合，制成一个简易的后台播放音乐的播放器。

---
+++ MyAndroidLibrary/src/broadcastreceiver/MusicService.java
@@ -0,0 +1,146 @@
+/**
+ * 
+ */
+package broadcastreceiver;
+
+import java.io.IOException;
+import java.sql.PreparedStatement;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.os.IBinder;
+
+/**
+ * @author CSH 2015-4-3 该次学习的过程中主要出现以下的几点的问题：
+ *         1.MusicService忘记在清单文件中注册了，可以分显式和隐式启动
+ *         2、broadcastReceive如果在activity和service组件代码里面动态注册了就不在需要再在清单里面再注册了
+ */
+public class MusicService extends Service {
+	ServiceReceive serviceReceive;
+	AssetManager assetam;
+	String[] musics = new String[] { "nw.mp3", "th.mp3", "hbp.mp3" };
+	MediaPlayer mPlayer;
+	int current = 0;
+	// 当前的状态，0x11代表没有播放，0X12代表正在播放，0x13代表暂停
+	private int status = 0x11;
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+
+		assetam = getAssets();
+		serviceReceive = new ServiceReceive();
+		IntentFilter filter = new IntentFilter();
+		// 设置本service中广播接收器监听的action为：broadcast.action.UPDATE_ACTION
+		filter.addAction(MusicServiceBroadReceiveActivity.CTL_ACTION);
+		registerReceiver(serviceReceive, filter);
+		// 创建mediaplayer
+		mPlayer = new MediaPlayer();
+		// 为mplayer播放完成事件绑定监听器
+		mPlayer.setOnCompletionListener(new OnCompletionListener() {
+
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				// TODO Auto-generated method stub
+				current++;
+				if (current >= 3) {
+					current = 0;
+				}
+				// 发送广播通知activity更改文本框
+				Intent uiIntent = new Intent(
+						MusicServiceBroadReceiveActivity.UPDATE_ACTION);
+				uiIntent.putExtra("current", current);
+				// 发送广播，将被Activity组件中的Broadcastreceive接收到
+				sendBroadcast(uiIntent);
+				// 准备播放下一首歌曲
+				prepareAndPlay(musics[current]);
+			}
+		});
+		super.onCreate();
+		System.out.println("service onCreat()......");
+	}
+
+	public class ServiceReceive extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO Auto-generated method stub
+			int control = intent.getIntExtra("control", -1);
+			switch (control) {
+			// 播放或暂停
+			case 1:
+				// 原来处于没有播放状态
+				if (status == 0x11) {
+					// 准备并播放音乐
+					prepareAndPlay(musics[current]);
+					status = 0x12;
+				}
+				// 原来处于播放状态
+				else if (status == 0x12) {
+					// 暂停
+					mPlayer.pause();
+					// 改为暂停状态
+					status = 0x13;
+				}
+				// 原来处于暂停状态
+				else if (status == 0x13) {
+					// 播放
+					mPlayer.start();
+					// 改为播放状态
+					status = 0x12;
+				}
+				break;
+			case 2:
+				// 如果原来正处在播放或暂停状态
+				if (status == 0x12 || status == 0x13) {
+					// 停止播放
+					mPlayer.stop();
+					status = 0x11;
+				}
+			
+			}
+			Intent uiIntent = new Intent(
+					MusicServiceBroadReceiveActivity.UPDATE_ACTION);
+			uiIntent.putExtra("update", status);
+			uiIntent.putExtra("current", current);
+			// 发送广播，将被Activity组件中的Broadcastreceive接收到
+			sendBroadcast(uiIntent);
+		}
+	}
+
+	/**
+	 * @param string
+	 */
+	private void prepareAndPlay(String music) {
+		// TODO Auto-generated method stub
+		try {
+			// 打开assets文件中指定的文件
+			AssetFileDescriptor afd = assetam.openFd(music);
+			mPlayer.reset();
+			// 使用MediaPlayer加载指定的音乐文件
+			mPlayer.setDataSource(afd.getFileDescriptor(),
+					afd.getStartOffset(), afd.getLength());
+			// 准备声音
+			mPlayer.prepare();
+			// 播放
+			mPlayer.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ MyAndroidLibrary/src/broadcastreceiver/MusicServiceBroadReceiveActivity.java
@@ -0,0 +1,112 @@
+package broadcastreceiver;
+
+import com.example.myandroidlibrary.R;
+import com.example.myandroidlibrary.R.layout;
+import com.example.myandroidlibrary.R.menu;
+
+import android.R.string;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * @author CSH 2015-4-3
+ */
+public class MusicServiceBroadReceiveActivity extends Activity implements
+		OnClickListener {
+	// 歌曲标题、歌手文本框
+	private TextView title, singer;
+	// 播放、暂停按钮，
+	private Button stop, play;
+	// 定义音乐的播放状态，0x11代表没有播放，0X12代表正在播放，0x13代表暂停
+	private int status = 0x11;
+	private BroadcastReceiveService receiver;
+	String[] titles = new String[] { "你我", "桐花", "黑白配" };
+	String[] singers = new String[] { "陈树辉", "李妙", "林小妹" };
+	// 这是后台service中的广播接收器所监听的action
+	public static final String CTL_ACTION = "broadcast.action.CTL_ACTION";
+	// 这是前台activity中的广播接收器所监听的action
+	public static final String UPDATE_ACTION = "broadcast.action.UPDATE_ACTION";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_music_service_broad_receive);
+		title = (TextView) findViewById(R.id.title);
+		singer = (TextView) findViewById(R.id.singer);
+		stop = (Button) findViewById(R.id.stop);
+		play = (Button) findViewById(R.id.play);
+		play.setOnClickListener(this);
+		stop.setOnClickListener(this);
+
+		receiver = new BroadcastReceiveService();
+		IntentFilter filter = new IntentFilter();
+		// 设置本activity中广播接收器监听的action为：broadcast.action.UPDATE_ACTION
+		filter.addAction(UPDATE_ACTION);
+		// 为本activity 动态注册下面内部类的BroadcastReceiveService
+		registerReceiver(receiver, filter);
+		System.out.println("activity  oncreat()...1");
+		// 启动后台音乐服务
+		Intent intent = new Intent(this, MusicService.class);
+		startService(intent);
+		System.out.println("activity  oncreat()...2");
+	}
+
+	public class BroadcastReceiveService extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO Auto-generated method stub
+			// 获取intent中的update消息，update代表播放状态
+			int update = intent.getIntExtra("update", -1);
+			// 获取intent中的current消息，current代表当前正在播放的歌曲
+			int current = intent.getIntExtra("current", -1);
+			if (current >= 0) {
+				title.setText(titles[current]);
+				singer.setText(singers[current]);
+			}
+			switch (update) {
+			case 0x11:
+				//没有播放的状态按钮设置为：播放
+				status = 0x11;
+				play.setText("播放");
+				break;
+			case 0x12:
+				//播放状态中按钮设置为：暂停
+				play.setText("暂停");
+				status = 0x12;
+				break;
+			case 0X13:
+				//暂停状态中按钮设置为：播放
+				play.setText("播放");
+				status = 0x13;
+				break;
+			}
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Intent intent = new Intent("broadcast.action.CTL_ACTION");
+		switch (v.getId()) {
+		case R.id.play:
+			intent.putExtra("control", 1);
+			System.out.println("单击播放按钮了");
+			break;
+		case R.id.stop:
+			intent.putExtra("control", 2);
+			break;
+		}
+		// 发送广播，将被Service组件中的BroadcastReceive接收到
+		sendBroadcast(intent);
+	}
+}
